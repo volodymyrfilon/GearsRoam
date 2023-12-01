ab3b9aed2ac9a77797fecc1b778bbdca20a0390f
Volodymyr <volodymyrfilon1312@gmail.com>
2023-12-01 18:55:27 +0100

    project init

---
+++ components/ui/Route.tsx
@@ -0,0 +1,23 @@
+import clsx from 'clsx'
+import Link from 'next/link'
+
+interface routeProps {
+	route: string
+	label: string
+	isActive: boolean
+	onClick: () => void
+}
+
+const Route = ({ route, label, isActive, onClick }: routeProps) => {
+	return (
+		<Link
+			href={route}
+			onClick={onClick}
+			className={clsx(isActive && 'text-primary rounded-none bg-transparent')}
+		>
+			{label}
+		</Link>
+	)
+}
+
+export default Route
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#eb5e28',
+				secondary: '#252422',
+				tertiary: '#403d39',
+				light: '#fffcf2',
+			},
+		},
+	},
+	plugins: [],
 }
 export default config
