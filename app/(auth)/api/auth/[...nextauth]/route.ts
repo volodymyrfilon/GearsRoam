9bf83841fcd4888825019b07b51eea0b69aac959
Volodymyr <volodymyrfilon1312@gmail.com>
2023-12-05 17:47:16 +0100

    feat: nextAuth/prisma

---
+++ app/(auth)/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+import prisma from '@/lib/prismadb'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import NextAuth, { AuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: AuthOptions = {
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		}),
+	],
+	debug: process.env.NODE_ENV === 'development',
+	secret: process.env.NEXTAUTH_SECRET,
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
