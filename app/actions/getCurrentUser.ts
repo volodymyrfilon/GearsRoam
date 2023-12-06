15ca6eea19c04800bca03618e4fc447b79939e24
Volodymyr <volodymyrfilon1312@gmail.com>
2023-12-06 18:10:59 +0100

    feat: get user/session

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,29 @@
+import prisma from '@/lib/prismadb'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../(auth)/api/auth/[...nextauth]/route'
+
+export async function getSession() {
+	return await getServerSession(authOptions)
+}
+
+export default async function getCurrentUser() {
+	try {
+		const session = await getSession()
+
+		if (!session?.user?.email) {
+			return null
+		}
+
+		const currentUser = await prisma.user.findUnique({
+			where: {
+				email: session.user.email as string,
+			},
+		})
+
+		if (!currentUser) {
+			return null
+		}
+	} catch (error: any) {
+		return null
+	}
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/shared/Navbar'
 import { AuthContext } from '@/context/AuthContext'
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
+import getCurrentUser from './actions/getCurrentUser'
 import './globals.css'
 
 const roboto = Roboto({
@@ -15,16 +16,17 @@ export const metadata: Metadata = {
 	description: 'Explore Safari with us',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: {
 	children: React.ReactNode
 }) {
+	const user = await getCurrentUser()
 	return (
 		<html lang='en'>
 			<AuthContext>
 				<body className={`${roboto.className} overflow-x-hidden bg-light`}>
-					<Navbar />
+					<Navbar user={user} />
 					{children}
 					<Footer />
 				</body>
