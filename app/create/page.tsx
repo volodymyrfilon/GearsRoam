14cc822be228376516f75830327660f6ba150fe3
Volodymyr <volodymyrfilon1312@gmail.com>
2023-12-07 17:45:19 +0100

    feat: image drop/create form

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,5 +1,10 @@
-const page = () => {
-	return <div>page</div>
+import CreateForm from '@/components/shared/CreateForm'
+import getCurrentUser from '../actions/getCurrentUser'
+
+const page = async () => {
+	const user = await getCurrentUser()
+
+	return <CreateForm user={user} />
 }
 
 export default page
--- components/shared/CreateForm.tsx
+++ components/shared/CreateForm.tsx
@@ -1,5 +1,42 @@
-const CreateForm = () => {
-	return <div>CreateForm</div>
+'use client'
+
+import { useEdgeStore } from '@/lib/edgestore'
+import { useEffect, useState } from 'react'
+
+const CreateForm = ({ user }) => {
+	const [file, setFile] = useState<File>()
+	const { edgestore } = useEdgeStore()
+	const [imagePath, setImagePath] = useState('')
+
+	const uploadImageHandler = async () => {
+		if (file) {
+			const res = await edgestore.publicFiles.upload({
+				file,
+			})
+			setImagePath(res.url)
+		}
+	}
+
+	useEffect(() => {
+		if (file) {
+			uploadImageHandler()
+		}
+	}, [file])
+
+	return (
+		<div className='mt-8 mx-auto w-full max-w-3xl px-4'>
+			<div className='bg-white py-8 shadow rounded-lg px-10'>
+				<h1 className='text-center text-2xl font-extrabold mb-10'>
+					Create a Post ✍️
+				</h1>
+				{!user ? (
+					<h2 className='text-center text-xl font-extrabold uppercase'>
+						Please Sign up or Log in to create a post!
+					</h2>
+				) : null}
+			</div>
+		</div>
+	)
 }
 
 export default CreateForm
