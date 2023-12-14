d0e92322514050f4bbbc72a4aebaad62f77fab5f
Volodymyr <volodymyrfilon1312@gmail.com>
2023-12-14 16:53:22 +0100

    fix: export authOptions

--- app/auth/api/auth/[...nextauth]/route.ts
+++ app/auth/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,5 @@
-import prisma from '@/lib/prismadb'
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import NextAuth, { AuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-
-const authOptions: AuthOptions = {
-	adapter: PrismaAdapter(prisma),
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID as string,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-		}),
-	],
-	secret: process.env.NEXTAUTH_SECRET,
-}
+import NextAuth from 'next-auth'
+import authOptions from './options' // Update the path accordingly
 
 const handler = NextAuth(authOptions)
-
 export { handler as GET, handler as POST }
